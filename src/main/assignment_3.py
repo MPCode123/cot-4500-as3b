50f260dbdb92f30d099cb0e82fa3da25eb3f20b7
MPCode123 <mq.pham2003@gmail.com>
2025-04-01 15:09:13 -0400

    Update assignment_3.py

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -1,7 +1,7 @@
 import numpy as np 
 
 def GaussAndBack(A,b):
-            x = np.linalg.solve(A,b)
+            x = np.linalg.solve(A,b) # Solve matrix equation
 
             print(x) # Solution
             
@@ -27,14 +27,14 @@ def Diagon_Domin(A):
                 diagonal = abs(A[i,i]) # Represents numbers diagonal in any matrix
                 non_diagonal_sum = np.sum(np.abs(A[i])) - diagonal # Sum of other numbers in that row
                 
-            if (diagonal < non_diagonal_sum):
+            if (diagonal < non_diagonal_sum): # If the absolute value of diagonal is less than sum of other row entries
                 print("The matrix is NOT diagonally dominant")
             else:
                 print("The matrix is diagonally dominant")
                 
 def Positive_Definite(A):
-            if (A == A.T).all():
-                if (np.all(np.linalg.eigvals(A) > 0)):
+            if (A == A.T).all(): # If matrix is symmetric, do this...
+                if (np.all(np.linalg.eigvals(A) > 0)): # If the eigenvalues are positive
                     print("The matrix is positive definite")
                 else:
                     print("The matrix is NOT positive definite")
