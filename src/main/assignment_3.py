ac4652b6b39c32a747863e3ebe6daa5cc1ae3376
MPCode123 <mq.pham2003@gmail.com>
2025-04-01 13:22:32 -0400

    Update assignment_3.py

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -1,39 +1,7 @@
 import numpy as np 
 
 def GaussAndBack(A,b):
-            n = len(A)
-            # Convert A,b to augmented matrix
-            aug_matrix = np.hstack([A, b.reshape(-1, 1)])
-
-            for i in range(n):
-                # Partial Pivoting: Swap rows if needed
-                max_row = i
-                max_val = abs(aug_matrix[i][i])
-                
-                for k in range(i + 1, n):
-                    if abs(aug_matrix[k][i] > max_val):
-                        mas_val = abs(aug_matrix[k][i])
-                        max_row = k
-            
-                # Make the pivot 1 (row normalization)
-                pivot = aug_matrix[i, i]
-                if pivot == 0: # Failsafe if divide by 0
-                    print("Can't Divide by 0")
-                aug_matrix[i] = aug_matrix[i] / pivot
-            
-            # Make zeros below pivot
-                for j in range(i + 1, n):
-                    factor = aug_matrix[j][i]
-                    aug_matrix[j] = aug_matrix[j] - (factor * aug_matrix[i])
-
-            # Back Substitution
-            x = np.zeros(n)
-                                        
-            for i in range(n-1, -1, -1):
-                x[i] = aug_matrix[i][-1]
-        
-                for j in range(i+1, n):
-                    x[i] = x[i] - aug_matrix[i][j] * x[j]
+            x = np.linalg.solve(A,b)
 
             print(x) # Solution
             
